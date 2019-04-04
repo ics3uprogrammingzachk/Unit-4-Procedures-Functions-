5c8dfb0efdaced894ec2ef666cec90603489a4be
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-04-04 12:57:04 -0400

    added temp converter

---
+++ TempConverterZachK/TempConverterZachK/TempConverterForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TempConverterZachK
+{
+    partial class TempConverterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(15, 108);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(12, 80);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(155, 13);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Enter the temperature in celsius";
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Location = new System.Drawing.Point(96, 113);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(13, 13);
+            this.lblFahrenheit.TabIndex = 2;
+            this.lblFahrenheit.Text = "?";
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(172, 80);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtTemp.TabIndex = 3;
+            // 
+            // TempConverterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "TempConverterForm";
+            this.Text = "Temp Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.TextBox txtTemp;
+    }
+}
+
---
+++ TempConverterZachK/TempConverterZachK/TempConverterForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by: Zachary Kranabetter
+ * Created on: April 4th 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Temp Converter
+ * This program gets a user inputted temperature in Celsius and converts it to Fahrenheit
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverterZachK
+{
+    public partial class TempConverterForm : Form
+    {
+        public TempConverterForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ConvertToFahrenheit (double temperature)
+        {
+            // convert the temperature to Fahrenheit
+            temperature = (temperature * 9 / 5 ) + 32;
+
+            // display thetemperature in Fahrenheit
+            lblFahrenheit.Text = "the temperature in Fahrenheit = " + Convert.ToString(temperature) + "°";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // get the temperature in Celsius
+            double temperature = Convert.ToDouble(txtTemp.Text);
+
+            // call the convert function
+            this.ConvertToFahrenheit(temperature);
+        }
+    }
+}
